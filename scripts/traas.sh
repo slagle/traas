7a96e16234773cd0d132fe3f179bb0d6c30b776d
James Slagle <jslagle@redhat.com>
2017-02-21 21:20:15 -0500

    Initial Commit

--- scripts/traas.sh
+++ scripts/traas.sh
@@ -2,21 +2,57 @@
 
 set -eux
 
-export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 export TRIPLEO_ROOT=$HOME/tripleo-root
-export PRIMARY_NODE_IP=192.0.2.19
-export SUB_NODE_IPS="192.0.2.20 192.0.2.18"
-export TOCI_JOBTYPE="nonha"
-
 mkdir -p $TRIPLEO_ROOT
+
+LOGFILE=$TRIPLEO_ROOT/traas.log
+exec > >(tee -a $LOGFILE)
+exec 2>&1
+
 cd $TRIPLEO_ROOT
 
-sudo yum -y install git
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+export TOCI_JOBTYPE=${TOCI_JOBTYPE:-"multinode-nonha-oooq"}
+export MTU=1400
+export DO_SETUP_NODEPOOL_FILES=${DO_SETUP_NODEPOOL_FILES:-"1"}
+export PRIMARY_NODE_IP=${PRIMARY_NODE_IP:-""}
+export SUB_NODE_IPS=${SUB_NODE_IPS:-""}
+export TOCI_JOBTYPE=${TOCI_JOBTYPE:-""}
+export SSH_OPTIONS=${SSH_OPTIONS:-'-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=Verbose -o PasswordAuthentication=no -o ConnectionAttempts=32 -i ~/.ssh/id_rsa'}
+
+
+function check_var {
+    local var_name=$1
+    eval local value=\$$var_name
+
+    if [ -z "$value" ]; then
+        echo "Variable $var_name is not defined"
+        exit 1
+    fi
+}
+
+check_var PRIMARY_NODE_IP
+check_var SUB_NODE_IPS
+check_var TOCI_JOBTYPE
+
+# Update openssh before the test builds are installed by tripleo-ci
+# See https://review.openstack.org/#/c/437683/
+sudo yum -y update openssh
+
+rpm -q git || sudo yum -y install git
 
 if [ ! -d tripleo-ci ]; then
-    git clone https://git.openstack.org/openstack-infra/tripleo-ci
+    git clone -b traas https://github.com/slagle/tripleo-ci
+fi
+if [ ! -d tripleo-quickstart ]; then
+    git clone https://git.openstack.org/openstack/tripleo-quickstart
+fi
+if [ ! -d tripleo-quickstart-extras ]; then
+    git clone https://git.openstack.org/openstack/tripleo-quickstart-extras
 fi
 
-$TRIPLEO_ROOT/tripleo-ci/scripts/tripleo.sh --setup-nodepool-files
+if [ "$DO_SETUP_NODEPOOL_FILES" = "1" ]; then
+    $TRIPLEO_ROOT/tripleo-ci/scripts/tripleo.sh --setup-nodepool-files
+fi
 
-$TRIPLEO_ROOT/tripleo-ci/toci_instack_oooq_multinode.sh
+$TRIPLEO_ROOT/tripleo-ci/toci_gate_test.sh
